ee98bf19b0ff981b48ca0fb19d072ceb91cda9d7
GabrielOppermann <gabriel.oppermann1997@gmail.com>
2024-06-27 19:50:00 +0200

    added to pipeline upload of model to S3

--- src/inference_pipeline/inference.py
+++ src/inference_pipeline/inference.py
@@ -2,26 +2,22 @@ import os
 import json
 import boto3
 import numpy as np
-import joblib
-from sagemaker import get_execution_role
-import keras 
+import tensorflow as tf
 
-# Define global variables to hold the model and min-max scaling parameters
-model = None
+# Define global variables to hold the min-max scaling parameters
 min_max = None
 
-def load_model_and_params(model_dir):
-    global model, min_max
-    if model is None or min_max is None:
-        # Load the model
-        model = joblib.load(os.path.join(model_dir, "model.joblib"))
-        
-        # Load the min_max.json
+def load_min_max_params():
+    global min_max
+    if min_max is None:
+        # Load the min_max.json from S3
         s3 = boto3.client('s3')
         bucket_name = 'blazemodelregistry'
-        key = os.path.join(model_dir, "min_max.json")
-        s3.download_file(bucket_name, key, '/tmp/min_max_values.json')
-        with open('/tmp/min_max_values.json', 'r') as f:
+        min_max_key = 'optimizer/latest/min_max_values.json'
+
+        min_max_path = '/tmp/min_max_values.json'
+        s3.download_file(bucket_name, min_max_key, min_max_path)
+        with open(min_max_path, 'r') as f:
             min_max = json.load(f)
 
 # Preprocess the incoming data
@@ -33,14 +29,17 @@ def input_fn(request_body, request_content_type):
         raise ValueError(f"Unsupported content type: {request_content_type}")
 
 def preprocess(input_data):
-    global min_max
-    # # Apply min-max scaling to the input data
+    # Load min_max_values.json if not already loaded
+    load_min_max_params()
+    # Dummy preprocessing, return the input data as-is
     return input_data
 
 # Make inference
 def predict_fn(input_data):
-    global model
-    predictions = model.predict(input_data)
+    # Use a dummy input for inference testing
+    keras_data = np.ones(2, 21, 10).astype(np.float32)
+    predict = model.signatures['serving_default']
+    predictions = predict(tf.constant(keras_data))['output_0'].numpy()
     return predictions
 
 # Postprocess the data
@@ -51,9 +50,11 @@ def output_fn(prediction, content_type):
         raise ValueError(f"Unsupported content type: {content_type}")
 
 def handler(event, context):
-    # Load the model and min_max.json if not already loaded
-    model_dir = os.environ['MODEL_DIR']
-    load_model_and_params(model_dir)
+    global model
+    # Load the model from the default SageMaker model directory
+    if model is None:
+        model_dir = '/opt/ml/model'
+        model = tf.saved_model.load(model_dir)
     
     # Get the request data
     request_body = event['body']
@@ -61,7 +62,7 @@ def handler(event, context):
     
     # Preprocess the data
     input_data = input_fn(request_body, request_content_type)
-    print("Input data arrived", input_data)
+    print("Input data arrived:", input_data)
     preprocessed_data = preprocess(input_data)
     
     # Make predictions
